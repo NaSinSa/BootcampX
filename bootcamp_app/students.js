725328732d1f50c7e317a3dff577b427b435902f
NaSinSa <andy.k.oh@hotmail.com>
2020-03-04 18:38:35 -0500

    security upgraded

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,15 +11,15 @@ const cohortName = process.argv.slice(2)[0];
 const howManyToShow = process.argv.slice(2)[1];
 
 const values = [`%${cohortName}%`, howManyToShow];
-
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
+WHERE cohorts.name LIKE $1
 ORDER BY students.id
-LIMIT ${howManyToShow};
-`)
+LIMIT $2;
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
